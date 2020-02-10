c75ebcdda13cab4c380cb611657275bf5dc694c6
Saheed Yusuf <yusufsaheedtaiwo@yahoo.com>
2020-02-10 15:38:27 +0100

    update on socket event handler for private chat

--- socket/events-handler.js
+++ socket/events-handler.js
@@ -1,38 +1,73 @@
 var connectedUsers = [];
 var socketIds = [];
 
-setInterval(()=>{
+setInterval(() => {
     console.log(connectedUsers.length)
 }, 1000)
 
+const getUserSocketsToPushTo = (username) => {
+    var userSocketsToReceive = [];
+    connectedUsers.map(user => {
+        if (user.username === username) {
+            userSocketsToReceive.push(user.socketId);
+        }
+    });
+
+    console.log("Number of users to push to " + userSocketsToReceive.length);
+    return userSocketsToReceive;
+}
 const IOEvents = (io) => {
 
     io.on('connection', socket => {
 
         io.emit('connection-received', {})
-    
+
         socket.on('join', data => {
-            if (socketIds.includes(socket.id)){
+            if (socketIds.includes(socket.id)) {
                 console.log("Socket already exists");
                 return;
-            } else{
+            } else {
                 socket.username = data.username;
                 connectedUsers[socket.username] = socket;
                 var userObject = {
-                    username : data.username,
-                    socketId : socket.id,
-                    name : data.first_name + ' ' + data.last_name
+                    username: data.username,
+                    socketId: socket.id,
+                    name: data.first_name + ' ' + data.last_name
                 };
-            if (!socketIds.includes(socket.id)){
-                connectedUsers.push(userObject);
-                socketIds.push(socket.id);
-            }
+                if (!socketIds.includes(socket.id)) {
+                    connectedUsers.push(userObject);
+                    socketIds.push(socket.id);
+                }
                 io.emit('all-users', connectedUsers);
             }
-            
+
         });
-    
-        socket.on('disconnect', ()=> {
+
+        socket.on('private-message-sent', data => {
+            console.dir(data);
+            //dispatch to the sender as well, so all connected sockets for that user can have their chat history updated
+
+            io.to(socket.id).emit('private-message-received', data);
+
+            var socketsToPushMessageTo = getUserSocketsToPushTo(data.fromUsername);
+            console.log('Number of sockets to send to ' + getUserSocketsToPushTo(data.fromUsername));
+            socketsToPushMessageTo.map(socketId => {
+                socket.broadcast.to(socketId).emit('private-message-received', data);
+                console.log("Emitted to " + socketId);
+            })
+
+
+            socketsToPushMessageTo = getUserSocketsToPushTo(data.toUsername);
+            console.log('Number of sockets to send to ' + getUserSocketsToPushTo(data.toUsername));
+            socketsToPushMessageTo.map(socketId => {
+                socket.broadcast.to(socketId).emit('private-message-received', data);
+                console.log("Emitted to " + socketId);
+            })
+            // io.emit('private-message-received', data);
+            // console.log("Emmitted");
+        });
+
+        socket.on('disconnect', () => {
             connectedUsers = connectedUsers.filter(user => {
                 return user.socketId !== socket.id;
             });
@@ -41,9 +76,9 @@ const IOEvents = (io) => {
             });
             io.emit('all-users', connectedUsers);
         });
-    
+
     });
-    
+
 }
 
 module.exports = IOEvents;
